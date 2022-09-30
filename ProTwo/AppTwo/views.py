0640ab07e06fe645614489e6fd5461a598298450
Muzahir7 <muzahirh@smu.edu>
2022-09-30 14:44:06 -0500

    rendering static files done for project 1

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-# def index(request):
-#     return HttpResponse("<h1>My Second App</h1>")
+def index(request):
+    return HttpResponse("<h1>My Second App</h1>")
 def help(request):
     my_dict = {'insert_here':"I suppose to render in the help view"}
     return render(request,'AppTwo/help.html',context=my_dict)
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = Path.joinpath(BASE_DIR, 'templates')
+STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
 # print(Path.joinpath(BASE_DIR, 'templates')) join path using pathlib that works for all os (addition from my side)
 
@@ -120,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
