48d193a76ee7f93081e6eaff0fc190590808febe
Muzahir7 <muzahirh@smu.edu>
2022-10-01 17:02:45 -0500

    created models for the data base

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+# Models are Basically database tables. to create a database table we have to create a model and
+# Model is created using the class key word and inheret from the parent class models.model
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class WebPage(models.Model):
+    # topic = models.ForeignKey(Topic) in older versions of Django it does on_delete=CASCADE by default
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=264, unique=True)
+    ulr = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(WebPage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
