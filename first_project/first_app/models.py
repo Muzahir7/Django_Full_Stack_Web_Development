2b05812906ce4ea17ef17710ea8762533e193196
Muzahir7 <muzahirh@smu.edu>
2022-10-02 00:12:50 -0500

    manipulated models QuerySets using python shell and django admin interface

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -13,10 +13,10 @@ class WebPage(models.Model):
     # topic = models.ForeignKey(Topic) in older versions of Django it does on_delete=CASCADE by default
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=264, unique=True)
-    ulr = models.URLField(unique=True)
+    url = models.URLField(unique=True)
 
     def __str__(self):
-        return self.top_name
+        return self.name
 
 class AccessRecord(models.Model):
     name = models.ForeignKey(WebPage, on_delete=models.CASCADE)
